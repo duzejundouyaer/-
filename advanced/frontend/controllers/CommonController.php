8db77b1889560143d6ab3cf0102bc50adde6784f
hwj <997242980@qq.com>
2017-02-13 14:05:07 +0800

    黄文靖提交

--- advanced/frontend/controllers/CommonController.php
+++ advanced/frontend/controllers/CommonController.php
@@ -15,7 +15,8 @@ class CommonController extends \yii\web\Controller
        $session=\Yii::$app->session;  //开启session
        $admin_session = $session['admin_name'];   //获取管理员session名称
        if(!isset($admin_session)){     //检测该原理元session是否存在
-			echo "<script>alert('请先登录');location.href='?r=login/login'</script>";die;
+			//echo "<script>alert('请先登录');location.href='?r=login/login'</script>";die;
+       	     return $this->redirect("?r=login/login");
 		}
     }
 
