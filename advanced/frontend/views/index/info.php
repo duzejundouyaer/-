ffd10c0b3718ab5ec3324a7d0109aa0a3dd9d685
hwj <997242980@qq.com>
2017-02-14 21:24:08 +0800

    黄文靖提交

--- advanced/frontend/views/index/info.php
+++ advanced/frontend/views/index/info.php
@@ -1,175 +1,292 @@
 <!DOCTYPE html>
-<html lang="zh-cn">
+<html >
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <meta name="renderer" content="webkit">
-    <title>网站信息</title>  
-    <link rel="stylesheet" href="css/pintuer.css">
-    <link rel="stylesheet" href="css/admin.css">
-    <script src="js/jquery.js"></script>
-    <script src="js/pintuer.js"></script>  
+<meta charset="UTF-8">
+<title></title>
+
+<script src="js/jrj6out.js"></script>
+<script>try{Typekit.load({ async: false });}catch(e){}</script>
+
+<style>
+  body {
+  position: relative;
+  margin: 0;
+  overflow: hidden;
+}
+
+.intro-container {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  color: white;
+  text-align: center;
+  margin: 0 auto;
+  right: 0;
+  left: 0;
+}
+
+h1 {
+  font-family: 'brandon-grotesque', sans-serif;
+  font-weight: bold;
+  margin-top: 0px;
+  margin-bottom: 0;
+  font-size: 20px;
+}
+@media screen and (min-width: 860px) {
+  h1 {
+    font-size: 40px;
+    line-height: 52px;
+  }
+}
+
+.fancy-text {
+  font-family: "adobe-garamond-pro",sans-serif;
+  font-style: italic;
+  letter-spacing: 1px;
+  margin-bottom: 17px;
+}
+
+.button {
+  position: relative;
+  cursor: pointer;
+  display: inline-block;
+  font-family: 'brandon-grotesque', sans-serif;
+  text-transform: uppercase;
+  min-width: 200px;
+  margin-top: 30px;
+}
+.button:hover .border {
+  box-shadow: 0px 0px 10px 0px white;
+}
+.button:hover .border .left-plane, .button:hover .border .right-plane {
+  transform: translateX(0%);
+}
+.button:hover .text {
+  color: #121212;
+}
+.button .border {
+  border: 1px solid white;
+  transform: skewX(-20deg);
+  height: 32px;
+  border-radius: 3px;
+  overflow: hidden;
+  position: relative;
+  transition: .10s ease-out;
+}
+.button .border .left-plane, .button .border .right-plane {
+  position: absolute;
+  background: white;
+  height: 32px;
+  width: 100px;
+  transition: .15s ease-out;
+}
+.button .border .left-plane {
+  left: 0;
+  transform: translateX(-100%);
+}
+.button .border .right-plane {
+  right: 0;
+  transform: translateX(100%);
+}
+.button .text {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  transition: .15s ease-out;
+}
+
+.x-mark {
+  right: 10px;
+  top: 10px;
+  position: absolute;
+  cursor: pointer;
+  opacity: 0;
+}
+.x-mark:hover .right {
+  transform: rotate(-45deg) scaleY(1.2);
+}
+.x-mark:hover .left {
+  transform: rotate(45deg) scaleY(1.2);
+}
+.x-mark .container {
+  position: relative;
+  width: 20px;
+  height: 20px;
+}
+.x-mark .left, .x-mark .right {
+  width: 2px;
+  height: 20px;
+  background: white;
+  position: absolute;
+  border-radius: 3px;
+  transition: .15s ease-out;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+}
+.x-mark .right {
+  transform: rotate(-45deg);
+}
+.x-mark .left {
+  transform: rotate(45deg);
+}
+
+.sky-container {
+  position: absolute;
+  color: white;
+  text-transform: uppercase;
+  margin: 0 auto;
+  right: 0;
+  left: 0;
+  top: 2%;
+  text-align: center;
+  opacity: 0;
+}
+@media screen and (min-width: 860px) {
+  .sky-container {
+    top: 18%;
+    right: 12%;
+    left: auto;
+  }
+}
+.sky-container__left, .sky-container__right {
+  display: inline-block;
+  vertical-align: top;
+  font-weight: bold;
+}
+.sky-container__left h2, .sky-container__right h2 {
+  font-family: 'brandon-grotesque', sans-serif;
+  font-size: 26px;
+  line-height: 26px;
+  margin: 0;
+}
+@media screen and (min-width: 860px) {
+  .sky-container__left h2, .sky-container__right h2 {
+    font-size: 72px;
+    line-height: 68px;
+  }
+}
+.sky-container__left {
+  margin-right: 5px;
+}
+.sky-container .thirty-one {
+  letter-spacing: 4px;
+}
+
+.text-right {
+  text-align: right;
+}
+
+.text-left {
+  text-align: left;
+}
+
+.twitter:hover a {
+  transform: rotate(-45deg) scale(1.05);
+}
+.twitter:hover i {
+  color: #21c2ff;
+}
+.twitter a {
+  bottom: -40px;
+  right: -75px;
+  transform: rotate(-45deg);
+}
+.twitter i {
+  bottom: 7px;
+  right: 7px;
+  color: #00ACED;
+}
+
+.social-icon a {
+  position: absolute;
+  background: white;
+  color: white;
+  box-shadow: -1px -1px 20px 0px rgba(0, 0, 0, 0.3);
+  display: inline-block;
+  width: 150px;
+  height: 80px;
+  transform-origin: 50% 50%;
+  transition: .15s ease-out;
+}
+.social-icon i {
+  position: absolute;
+  pointer-events: none;
+  z-index: 1000;
+  transition: .15s ease-out;
+}
+
+.youtube:hover a {
+  transform: rotate(45deg) scale(1.05);
+}
+.youtube:hover i {
+  color: #ec4c44;
+}
+.youtube a {
+  bottom: -40px;
+  left: -75px;
+  transform: rotate(45deg);
+}
+.youtube i {
+  bottom: 7px;
+  left: 7px;
+  color: #E62117;
+}
+
+</style>
+
+<script src="js/prefixfree.min.js"></script>
+
 </head>
+
 <body>
-<div class="panel admin-panel">
-  <div class="panel-head"><strong><span class="icon-pencil-square-o"></span> 网站信息</strong></div>
-  <div class="body-content">
-    <form method="post" class="form-x" action="">
-      <div class="form-group">
-        <div class="label">
-          <label>网站标题：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="stitle" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>网站LOGO：</label>
-        </div>
-        <div class="field">
-          <input type="text" id="url1" name="slogo" class="input tips" style="width:25%; float:left;" value="" data-toggle="hover" data-place="right" data-image=""  />
-          <input type="button" class="button bg-blue margin-left" id="image1" value="+ 浏览上传" >
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>网站域名：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="surl" value="" />
-        </div>
-      </div>
-      <div class="form-group" style="display:none">
-        <div class="label">
-          <label>副加标题：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="sentitle" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>网站关键字：</label>
-        </div>
-        <div class="field">
-          <textarea class="input" name="skeywords" style="height:80px"></textarea>
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>网站描述：</label>
-        </div>
-        <div class="field">
-          <textarea class="input" name="sdescription"></textarea>
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>联系人：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_name" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>手机：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_phone" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>电话：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_tel" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group" style="display:none;">
-        <div class="label">
-          <label>400电话：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_400" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>传真：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_fax" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>QQ：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_qq" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group" style="display:none">
-        <div class="label">
-          <label>QQ群：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_qqu" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-     
-      <div class="form-group">
-        <div class="label">
-          <label>Email：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_email" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label>地址：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_address" value="" />
-          <div class="tips"></div>
-        </div>
-      </div>  
-              
-      <div class="form-group">
-        <div class="label">
-          <label>底部信息：</label>
-        </div>
-        <div class="field">
-          <textarea name="scopyright" class="input" style="height:120px;"></textarea>
-          <div class="tips"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="label">
-          <label></label>
-        </div>
-        <div class="field">
-          <button class="button bg-main icon-check-square-o" type="submit"> 提交</button>
-        </div>
-      </div>
-    </form>
+<div class="x-mark">
+  <div class="container">
+    <div class="left"></div>
+    <div class="right"></div>
   </div>
 </div>
-</body></html>
+<div class="intro-container">
+  
+  <h1>Welecome to  <br> duoxue.com<br> admin center</h1>
+
+  <div class="button shift-camera-button">
+    <div class="border">
+      <div class="left-plane"></div>
+      <div class="right-plane"></div>
+    </div>  
+    <div class="text">点击进入</div>
+  </div>
+</div>
+<div class="sky-container">
+  <div class="text-right sky-container__left">
+    <h2 class="portfolio">
+      付出的努力和
+    </h2>
+    <h2 class="resurrection">
+      收获成
+    </h2>
+  </div>
+  <div class="text-left sky-container__right">
+    <h2 class="08">
+      永远
+    </h2>
+    <h2 class="thirty-one">
+      正比
+    </h2>
+    <h2 class="2016">
+      加油！！！
+    </h2>
+  </div>
+</div>
+
+
+<script src='js/jquery.min.js'></script>
+<script src='js/TweenMax.min.js'></script>
+<script src='js/three.min.js'></script>
+<script src="js/index.js"></script>
+
+</body>
+</html>
